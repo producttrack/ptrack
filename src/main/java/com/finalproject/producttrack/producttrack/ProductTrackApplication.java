ef88e98e6dc07b0ddb439f4c775088c105986928
screamcol <screamcol@gmail.com>
2020-07-28 20:55:47 +0300

    Initial project app

---
+++ src/main/java/com/finalproject/producttrack/producttrack/ProductTrackApplication.java
@@ -0,0 +1,21 @@
+package com.finalproject.producttrack.producttrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class ProductTrackApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ProductTrackApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductTrackApplication.class, args);
+	}
+}
---
+++ src/test/java/com/finalproject/producttrack/ProductTrackApplicationTests.java
@@ -0,0 +1,15 @@
+package com.finalproject.producttrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+//@SpringBootTest
+@ContextConfiguration
+class ProductTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
